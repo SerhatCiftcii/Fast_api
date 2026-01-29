38b83e7961d12f1c978497247b94fcd4a6428590
Serhat Çiftçi <s.ciftci561@gmail.com>
2026-01-29 18:31:00 +0300

    Refactor main.py to update index response and add blog routes

--- main.py
+++ main.py
@@ -2,11 +2,19 @@
 from fastapi import FastAPI
 
 app = FastAPI()
-
+# yukardan aşşağıya doğru olur herşeyi dkkatli kullan.
 @app.get('/')
 def index():
-    return {"data":{"name":"serhat"}}
+    return {"data":"blog list"}
 
-@app.get("/about")
-def about():
-    return {"data":"about page"}
+@app.get("/blog/{id}")
+def show(id:int):
+    return {"data":id}
+
+@app.get("/blog/unpublished")
+def unpublished():
+    return {"data":"all unpublished blogs"}
+@app.get("/blog/{id}/comments")
+def comments(id):
+    #fetch comments of blog with id =id
+    return {"data":{"1","2"}}
